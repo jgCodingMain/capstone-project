2cd87975f883592313225889d4de5dec645b081f
jgCodingMain <162503964+jgCodingMain@users.noreply.github.com>
2025-05-04 16:01:54 -0600

    updating button link

--- src/homepageComponents/Hero.js
+++ src/homepageComponents/Hero.js
@@ -12,7 +12,7 @@ function Hero() {
         <h1>Little Lemon</h1>
         <h3>Chicago</h3>
         <p>Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a
-          modern twist.</p><button><a href="/booking">Reserve a Table</a></button>
+          modern twist.</p><button><a href="https://jgcodingmain.github.io/capstone-project/#/booking">Reserve a Table</a></button>
       </div>
       <div class="gridParentContainer"><img src="https://jgcodingmain.github.io/capstone-project/assets/restauranfood.jpg" alt="Little Lemon appetizer" />
       </div>
